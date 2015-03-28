3684a1e4460ffd144d9083bc914fe7adbbf7ae15
Nate River <blackrainbowgames@gmail.com>
2015-03-28 23:22:37 +0300

    upd

---
+++ Assets/Scripts/Silhouette.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Silhouette : Script
+    {
+        public GameObject MoscowSilhouette;
+        public GameObject NYSilhouette;
+
+        public void Awake()
+        {
+            switch (Application.systemLanguage)
+            {
+                case SystemLanguage.Russian:
+                case SystemLanguage.Ukrainian:
+                case SystemLanguage.Belarusian:
+                    MoscowSilhouette.SetActive(true);
+                    NYSilhouette.SetActive(false);
+                    break;
+                default:
+                    MoscowSilhouette.SetActive(false);
+                    NYSilhouette.SetActive(true);
+                    break;
+            }
+        }
+    }
+}
